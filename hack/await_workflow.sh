5861b4416006142ba5d38d968f0d7e3d72731a2b
Jeffrey Limnardy <jeffrey.limnardy@sap.com>
2024-09-03 14:19:49 +0200

    fix script

--- hack/await_workflow.sh
+++ hack/await_workflow.sh
@@ -35,7 +35,7 @@ until [[ $status == "completed" && $conclusion == "success" ]]; do
     response=$(curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer <YOUR-TOKEN>" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/OWNER/REPO/check-runs)
 
